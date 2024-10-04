f4c19a623c59d89fa241d267297eae8adef50f1a
xGentuso <ryan.mota@triosstudent.com>
2024-10-04 10:21:29 -0400

    added html to users/profile.php

---
+++ app/views/users/profile.php
@@ -0,0 +1,13 @@
+<?php include '../includes/header.php'; ?>
+
+<?php if (isset($_SESSION['user_id'])): ?>
+  <h2>Profile</h2>
+  <p>Welcome, <?echo htmlspecialchars($user['name']); ?>!</p>
+  <p>Email: <?php echo htmlspecialchars(($user['email'])); ?></p>
+
+  <a href="/public/index.php?action=logout">Logout</a>
+<?php else: ?>
+  <p>You need to <a href="/public/index.php?action=login">login</a> to view this page.</p>
+<?php endif; ?>
+
+<?php include '../includes/footer.php'; ?>
